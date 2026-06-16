09ce0aba01fdab60917c91b003f872d7ddd2a6c8
Anirban Das <itsadas.04@gmail.com>
2026-06-16 17:05:41 +0530

    Create 1757. Recyclable and Low Fat Products.sql
    Added solution for Problem 1757

---
+++ 1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,6 @@
+q.1757. Recyclable and Low Fat Products
+ -----------------------------------------------
+Code:-
+      SELECT product_id
+      FROM Products
+      WHERE low_fats = 'Y' AND recyclable = 'Y';
